8dca610022bb44ad99f9177d206d6d2235ace999
halloamt <hallo.amt@gmx.de>
2021-01-11 16:59:50 +0100

    Update Radius.h
    Longer Radius Timeouts

--- src/Cedar/Radius.h
+++ src/Cedar/Radius.h
@@ -106,8 +106,8 @@
 #define	RADIUS_H
 
 #define	RADIUS_DEFAULT_PORT		1812			// The default port number
-#define	RADIUS_RETRY_INTERVAL	500				// Retransmission interval
-#define	RADIUS_RETRY_TIMEOUT	(10 * 1000)		// Time-out period
+#define	RADIUS_RETRY_INTERVAL	1000				// Retransmission interval
+#define	RADIUS_RETRY_TIMEOUT	(90 * 1000)		// Time-out period
 #define	RADIUS_INITIAL_EAP_TIMEOUT	1600		// Initial timeout for EAP
 
 
